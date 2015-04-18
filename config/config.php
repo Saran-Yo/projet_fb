49ffd6cdb9e68cbc171203bc0b6cb6e8c2180daa
unknown <saranjan@outlook.com>
2015-04-18 15:34:14 +0200

    url change

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
 	$appId='680552975386696';
 	$appSecret='d0569ecbb268d40623df03c75166a5b5';
-	$redirectUrl='https://find404.herokuapp.com';
+	$redirectUrl='https://find404.herokuapp.com/';
 	$permission=array('email','user_birthday,publish_actions');
 ?>
