b0e50503a7815790a0425a77cf338951202c7164
unknown <saranjan@outlook.com>
2015-04-23 00:21:54 +0200

    ajout ico

--- view/includes/header.php
+++ view/includes/header.php
@@ -3,9 +3,11 @@
 <head>
 	<meta charset="utf-8">
 	<title>find404</title>
+
 </head>
 	<link rel="stylesheet" type="text/css" href="./view/css/bootstrap.min.css" />
-	<link rel="stylesheet" type="text/css" href="./view//css/font-awesome.min.css" />
-	<link rel="stylesheet" type="text/css" href="./view//css/theme.css" />
-	<link rel="stylesheet" type="text/css" href="./view//css/style.css" />
+	<link rel="stylesheet" type="text/css" href="./view/css/font-awesome.min.css" />
+	<link rel="stylesheet" type="text/css" href="./view/css/theme.css" />
+	<link rel="stylesheet" type="text/css" href="./view/css/style.css" />
+	<link rel="shortcut icon" type="image/x-icon" href="./view/img/icone.ico" />
 <body>
