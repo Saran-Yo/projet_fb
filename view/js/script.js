160a69ee0c3b584c48392037686e94a1cc212ce6
unknown <saranjan@outlook.com>
2015-04-23 08:43:30 +0200

    pop up 4

--- view/js/script.js
+++ view/js/script.js
@@ -113,7 +113,7 @@ $(document).ready(function(){
 
         FB.ui({
           method: 'pagetab',
-          redirect_uri: 'https://find404.herokuapp.com/'
+          redirect_uri: 'https://www.facebook.com/find404'
         }, function(response){});
 
 
